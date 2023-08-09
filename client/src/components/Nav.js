7fd3b9205870ac76bfac5429940c40ce731de857
NadeemTalaat <nadeem.talaat@gmail.com>
2023-08-09 00:54:05 -0400

    Fixed navbar links to change url path, added some styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,17 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 function Nav(props) {
   return (
     <nav className="Nav">
       {props.pages.map((page) => {
         return (
-          <div
+          <Link
             className={`Nav-button ${
               props.currentPage.name === page.name ? "active" : ""
             }`}
             key={page.name}
+            to={page.path}
             onClick={() => props.setCurrentPage(page)}
           >
             {page.name}
-          </div>
+          </Link>
         );
       })}
     </nav>
