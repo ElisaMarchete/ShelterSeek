916631301509ad4c1af1d1f884190d7a7402ef2c
Peter Park <peterjpark@pm.me>
2023-08-10 03:06:18 -0400

    Add login and signup UI components using MUI

---
+++ client/src/components/AccountMenu/atoms/index.js
@@ -0,0 +1,5 @@
+import AccountMenuBtn from "./AccountMenuBtn";
+import LoggedInMenuList from "./LoggedInMenuList";
+import LoggedOutMenuList from "./LoggedOutMenuList";
+
+export { AccountMenuBtn, LoggedInMenuList, LoggedOutMenuList };
---
+++ client/src/components/AccountMenu/molecules/AccountMenuPopover.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Menu from "@mui/material/Menu";
+import { LoggedInMenuList, LoggedOutMenuList } from "../atoms";
+
+export default function AccountMenuPopover({
+  anchorEl,
+  open,
+  handleClose,
+  isLoggedIn,
+}) {
+  return (
+    <Menu
+      anchorEl={anchorEl}
+      id="account-menu"
+      open={open}
+      onClose={handleClose}
+      onClick={handleClose}
+      PaperProps={{
+        elevation: 0,
+        sx: {
+          overflow: "visible",
+          filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+          mt: 1.5,
+          "& .MuiAvatar-root": {
+            width: 32,
+            height: 32,
+            ml: -0.5,
+            mr: 1,
+          },
+          "&:before": {
+            content: '""',
+            display: "block",
+            position: "absolute",
+            top: 0,
+            right: 14,
+            width: 10,
+            height: 10,
+            bgcolor: "background.paper",
+            transform: "translateY(-50%) rotate(45deg)",
+            zIndex: 0,
+          },
+        },
+      }}
+      transformOrigin={{ horizontal: "right", vertical: "top" }}
+      anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+    >
+      {isLoggedIn
+        ? LoggedInMenuList(handleClose)
+        : LoggedOutMenuList(handleClose)}
+    </Menu>
+  );
+}
