7ee0044c37ef2ec665fe1d930978ec8b095f20c4
Marek Polak <markalexpolak@gmail.com>
2023-08-16 20:21:03 -0400

    added logic to image file '

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,34 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { Button, Typography, Paper } from '@mui/material';
 
-const ImageUpload = (updateUrl) => {
-  const [uploadedImageUrl, setUploadedImageUrl] = useState('');
-  const [formData, setFormData] = useState({image :(null)});
+const ImageUpload = ({ updateUrl }) => {
+  const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
+  const fileInputRef = useRef(null);
 
-  const handleFileChange = (event) => {
-    setFormData({image: event.target.files[0]});
-    handleUpload();
-  }
+  const handleFileChange = async () => {
+    const selectedFile = fileInputRef.current.files[0];
 
-
-  const handleUpload = async () => {
-    if (!formData.image) {
+    if (!selectedFile) {
       console.log('No file selected');
       return;
     }
-  
+
     try {
       const formDataToSend = new FormData();
-      formDataToSend.append('image', formData.image);
-  
+      formDataToSend.append('image', selectedFile);
+
       const response = await fetch('http://localhost:3001/getimg', {
         method: 'POST',
         body: formDataToSend,
       });
-  
+
       if (response.ok) {
-        const responseData = await response.json(); // Parse the JSON response
+        const responseData = await response.json();
         console.log('Image uploaded successfully:', responseData.imageUrl);
-        setUploadedImageUrl(responseData.imageUrl); // Store the image URL
+        setUploadedImageUrl(responseData.imageUrl);
         updateUrl(responseData.imageUrl);
       } else {
         console.error('Image upload failed');
@@ -40,22 +37,40 @@ const ImageUpload = (updateUrl) => {
 
   return (
     <div>
-      <input type="file" accept="image/*" name = "image" onChange={handleFileChange} />
+      <Button component="label" variant="contained" color="primary">
+        Upload Image
+        <input
+          type="file"
+          accept="image/*"
+          name="image"
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+      </Button>
 
       {uploadedImageUrl && (
-        <div>
-          <h2>Uploaded Image:</h2>
-          <img src={uploadedImageUrl} alt="Uploaded" style={{ maxWidth: '200px' }} />
-        </div>
+        <Paper elevation={3} style={{ padding: '16px', marginTop: '16px' }}>
+          <Typography variant="h6">Uploaded Image:</Typography>
+          <img
+            src={uploadedImageUrl}
+            alt="Uploaded"
+            style={{ maxWidth: '200px', marginTop: '8px' }}
+          />
+        </Paper>
       )}
     </div>
   );
 };
+
+/*
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleUpdateUrl = (newImageUrl) => {
+    setImageUrl(newImageUrl);
+  };*/
+
 export default ImageUpload;
-
-
-
-
 
 
 
